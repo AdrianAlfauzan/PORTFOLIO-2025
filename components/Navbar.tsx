e3eff1b1d474ca41bf546457a37f34d272277b8a
Adrian Musa Alfauzan <adrianmusaalfauzan@example.com>
2025-12-21 23:16:08 +0700

    RENAME NAVBAR

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -198,7 +198,7 @@ export default function Navbar() {
                 ) : (
                   <>
                     <User size={18} className="inline mr-2 align-middle" />
-                    Adrian Musa Alfauzan
+                    Adrian Alfauzan
                   </>
                 )}
               </span>
