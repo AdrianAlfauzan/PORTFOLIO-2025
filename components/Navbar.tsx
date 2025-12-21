2de3f1eed831173fc9610ff4092efe57e5a9d92f
Adrian Musa Alfauzan <adrianmusaalfauzan@example.com>
2025-12-21 23:54:57 +0700

    Update navbar text

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -198,7 +198,7 @@ export default function Navbar() {
                 ) : (
                   <>
                     <User size={18} className="inline mr-2 align-middle" />
-                    Adrian Alfauzan
+                    Adrian M.A
                   </>
                 )}
               </span>
